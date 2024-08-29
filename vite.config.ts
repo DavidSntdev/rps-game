ed76eb354f72145eb03b3454d199cc0f988cbf88
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-08-29 18:21:58 -0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/rps-game",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          animations: ["framer-motion"],
+          ui: ["@nextui-org/react", "react-icons"], 
+        },
+      },
+    },
+  },
 });
